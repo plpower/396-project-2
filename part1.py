ecc2c5be5a03e8fb1df119845259196f85ba5631
Patrice Power <patricepower2021@u.northwestern.edu>
2020-04-26 23:52:57 -0400

    Calculated regret for EW and FTPL

--- part1.py
+++ part1.py
@@ -10,66 +10,84 @@ import math
 # Tune learning rates & compare
 
 
-def exponential_weights(test_data, epsilon):
+def exponential_weights(test_data, epsilon, h):
     action1 = test_data[1]
     action2 = test_data[2]
     total_payoff = 0
 
-    for r in range(1, len(test_data[1])+1):
+    for r in range(len(test_data[1])):
         # calculate the probability of chosing every action in round r 
-        probabilities, payoffs = get_probabilities(r, epsilon, test_data)
+        probabilities, round_payoffs = get_probabilities(r, epsilon, h, test_data)
+        print(probabilities, round_payoffs)
         # chose action for round r with probabilities
-        action_payoff = np.random.choice(payoffs, p=probabilities)
+        action_payoff = np.random.choice(round_payoffs, p=probabilities)
+        print(action_payoff)
         total_payoff += action_payoff
 
     # regret is 2 * h sqrt(ln(k) / h)
     # learning rate is sqrt(ln(k) / h)
 
+    print("EW TOTAL PAYOFF", total_payoff)
     return total_payoff
 
-def get_probabilities(r, e, test_data):
-    payoffs = []
+def get_probabilities(r, e, h, test_data):
+    hindsight_payoffs = []
     total_payoff = 0
     probabilities = []
-    for action in range(len(test_data)):
-        action_payoff = sum(test_data[action + 1][:r])
-        payoffs.append(action_payoff)
-        print(test_data[action+1][:r])
-        print(action_payoff)
-        print(payoffs)
-    total_payoff = sum(payoffs)
-
-    for action in range(len(test_data)):
-        probabilities.append(payoffs[action]/total_payoff)
+    curr_payoffs = []
     
-    return probabilities, payoffs
+    if r == 0 :
+        for action in range(len(test_data)):
+            action_payoff = test_data[action + 1][0]
+            curr_payoffs.append(action_payoff)
+            hindsight_payoff = 0
+            hindsight_payoffs.append(hindsight_payoff)
+        return [0.5, 0.5], curr_payoffs
+    else:
+        for action in range(len(test_data)):
+            action_payoff = test_data[action + 1][r]
+            curr_payoffs.append(action_payoff)
+            hindsight_payoff = sum(test_data[action + 1][:r])
+            hindsight_payoffs.append((1+e) ** (hindsight_payoff/h))
+        total_payoff = sum(hindsight_payoffs)
+
+        for action in range(len(test_data)):
+            probabilities.append(hindsight_payoffs[action]/total_payoff)
+
+        return probabilities, curr_payoffs
 
 
-def follow_perturbed_leader(test_data):
+def follow_perturbed_leader(test_data, epsilon):
     # get values for each action at every round
-    action1 = test_data[1]
-    action2 = test_data[2]
+    action1 = test_data[1].copy()
+    action2 = test_data[2].copy()
 
     # generate hallucinations 
-    hallucinations = geom.rvs(p, size=len(test_data))
-
+    hallucinations = geom.rvs(epsilon, size=len(test_data))
+    print(hallucinations)
+    
     # add a halicination at round 0
     action1.insert(0, hallucinations[0])
     action2.insert(0, hallucinations[1])
-
+    
     # loop through and at each day choose the payoff of the BIH action
     # key here is that we take into account the round 0 hallucinations
     ftpl = 0
     for idx in range(len(action1)):
-        bih1, bih2 = best_in_hindsight(action1, action2, idx)
-        if bih2 > bih1:
-            ftpl += action2[idx]
+        if idx == 0:
+            continue
         else:
-            ftpl += action1[idx]
+            bih1, bih2 = best_in_hindsight(action1, action2, idx)
+            if bih2 > bih1:
+                ftpl += action2[idx]
+                print("action 2")
+            else:
+                ftpl += action1[idx]
+                print("action 1")
     
     # now we calculated payoff for FTPL
     # can further compare with OPT to get regret
-
+    print('FTPL TOTAL PAYOFF',ftpl)
     return ftpl
 
 def best_in_hindsight(action1, action2, curr_round):
@@ -88,6 +106,20 @@ def theo_opt_epsilon(test_data):
     
     return epsilon
 
+def calculate_regret(test_data, alg):
+    # calculate OPT
+    action_bihs = []
+    for action in test_data:
+        print(test_data[action])
+        action_bihs.append(sum(test_data[action]))
+    print("action_bihs", action_bihs)
+    best_bih = max(action_bihs)
+    
+    print(best_bih)
+    regret = (best_bih - alg) / len(test_data[1])
+    
+    return regret
+
 
 if __name__ == "__main__":
     test_data = {
@@ -98,9 +130,14 @@ if __name__ == "__main__":
     # ~calculate that~
     epsilon = theo_opt_epsilon(test_data)
     print(epsilon)
+    h = 1
     
     # implement the algorithms & calculate rergret
-    exponential_weights(test_data, epsilon)
-    follow_perturbed_leader(test_data)
-
+    ew = exponential_weights(test_data, epsilon, h)
+    ftpl = follow_perturbed_leader(test_data, epsilon)
+    
     # compare the regrets of these two alorithms againist each other
+    ew_regret = calculate_regret(test_data, ew)
+    print('ew regret', ew_regret)
+    ftpl_regret = calculate_regret(test_data, ftpl)
+    print('ftpl regret', ftpl_regret)
